d7407c8395829daeba193d9c67e54b3c0d9c0460
Liuliwei <1057878296@qq.com>
2019-03-18 21:30:14 +0800

    修改了主界面，并添加部分content界面

---
+++ app/src/main/java/com/example/focuson/InformationAdapter.java
@@ -0,0 +1,62 @@
+package com.example.focuson;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.focuson.db.Information;
+
+import java.util.List;
+
+public class InformationAdapter extends RecyclerView.Adapter<InformationAdapter.InformationViewHolder> {
+    private Context mContext;
+
+    private List<Information> inforList;
+
+    public InformationAdapter(List<Information> list)
+    {
+        inforList = list;
+    }
+
+    @NonNull
+    @Override
+    public InformationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if(mContext == null)
+        {
+            mContext = parent.getContext();
+        }
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_information, parent, false);
+        InformationViewHolder holder = new InformationViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public int getItemCount() {
+        return inforList.size();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InformationViewHolder holder, int position) {
+        Information infor = inforList.get(position);
+        holder.contentView.setText(infor.getContent());
+        CharSequence date = android.text.format.DateFormat.format(" yyyy.MM.dd  hh:mm", infor.getTime());
+        holder.timeView.setText(date);
+    }
+
+    public class InformationViewHolder extends RecyclerView.ViewHolder {
+        private TextView contentView;
+        private TextView timeView;
+
+        public InformationViewHolder(View itemView) {
+            super(itemView);
+            contentView = itemView.findViewById(R.id.content_view);
+            timeView = itemView.findViewById(R.id.time_view);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/focuson/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.focuson;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.focuson.db.Information;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<Information> list = new ArrayList<>();
+    private InformationAdapter adapter;
+    private TextView noFocusTextView;
+    private FloatingActionButton addFloatingButton;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21){
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        setContentView(R.layout.activity_main);
+        initComponent();
+        clickFab();
+        if(list.size() > 0)
+        {
+            noFocusTextView.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    /**
+     * 初始化主界面的控件
+     */
+    private void initComponent(){
+        recyclerView = findViewById(R.id.infor_recycler_view);
+        noFocusTextView = findViewById(R.id.no_focus_text_view);
+        addFloatingButton = findViewById(R.id.add_fab);
+        //recyclerView设置
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        Information info = new Information();
+        info.setContent("XXX");
+        Date date = new Date();
+        info.setTime(date);
+        list.add(info);
+        list.add(info);
+        list.add(info);
+        adapter = new InformationAdapter(list);
+        recyclerView.setAdapter(adapter);
+    }
+
+    /**
+     * Fab点击事件设置
+     */
+    private void clickFab(){
+        addFloatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ContentActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right , R.anim.slide_out_left);
+            }
+        });
+    }
+}
